032562ecfdb310ccd2ae5a30a495e30303dd00c7
Thibaut Dutartre <t.dutartre@gmail.com>
2019-10-04 09:58:13 +0200

    👀 Add ability to analyze bundles

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 const withMDX = require('@next/mdx')();
-
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx'],
-  exportPathMap: function() {
-    return {
-      '/': { page: '/' },
-      '/duple': { page: '/duple' },
-      '/refs': { page: '/refs' },
-    };
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
+
+module.exports = withBundleAnalyzer(
+  withMDX({
+    pageExtensions: ['js', 'jsx', 'mdx'],
+    exportPathMap: function() {
+      return {
+        '/': { page: '/' },
+        '/duple': { page: '/duple' },
+        '/refs': { page: '/refs' },
+      };
+    },
+  })
+);
